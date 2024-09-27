d310c9d851364bc6e219ddca81c7ddebad68d740
mcre <mc@mchs-u.net>
2024-09-27 23:42:37 +0900

    fontsourceを導入

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,12 @@
 import { registerPlugins } from "@/plugins";
 import { ViteSSG } from "vite-ssg/single-page";
 import App from "./App.vue";
+
 import "@/styles/global.scss";
+
+import "@fontsource/zen-maru-gothic/400.css";
+import "@fontsource/zen-maru-gothic/700.css";
+
 import { useRss } from "@/composables/useRss";
 
 export const createApp = ViteSSG(App, async (ctx) => {
