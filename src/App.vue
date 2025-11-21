e4a4c8d2d3b85ace3e2a22dddc99799c26210725
mcre <mc@mchs-u.net>
2025-11-21 18:53:39 +0900

    レイアウト調整

--- src/App.vue
+++ src/App.vue
@@ -20,21 +20,42 @@
         </v-row>
         <sns-row class="mt-6" />
 
-        <v-row class="mt-10" justify="center">
+        <!-- PC用レイアウト -->
+        <v-row v-if="mdAndUp" class="mt-10" justify="center">
+          <v-col cols="12" sm="10" md="6" lg="6" xl="4">
+            <profile-card class="mb-4" />
+            <zenn-card class="mb-4" />
+            <github-card class="mb-4" />
+            <website-card class="mb-4" />
+            <youtube-card class="mb-4" />
+          </v-col>
+          <v-col cols="12" sm="10" md="6" lg="6" xl="4">
+            <skill-card class="mb-4" />
+            <note-card class="mb-4" />
+            <waka-time-card class="mb-4"/>
+          </v-col>
+        </v-row>
+
+        <!-- スマホ用レイアウト -->
+        <v-row v-else class="mt-10" justify="center">
           <v-col cols="12" sm="10" md="6" lg="6" xl="4">
             <profile-card class="mb-4" />
             <skill-card class="mb-4" />
+            <zenn-card class="mb-4" />
+            <note-card class="mb-4"/>
+            <github-card class="mb-4" />
             <website-card class="mb-4" />
             <youtube-card class="mb-4" />
-            <waka-time-card />
-          </v-col>
-          <v-col cols="12" sm="10" md="6" lg="6" xl="4">
-            <github-card class="mb-4" />
-            <zenn-card class="mb-4" />
-            <note-card />
+            <waka-time-card class="mb-4" />
           </v-col>
         </v-row>
       </v-container>
     </v-main>
   </v-app>
 </template>
+
+<script setup>
+import { useDisplay } from "vuetify";
+
+const { mdAndUp } = useDisplay();
+</script>
