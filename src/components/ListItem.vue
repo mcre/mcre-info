b8c5d4a6066fd722c8ff461d5e23e3013c4652cc
mcre <mc@mchs-u.net>
2025-11-22 17:26:05 +0900

    ProfileとSkillを書き直した

--- src/components/ListItem.vue
+++ src/components/ListItem.vue
@@ -1,8 +1,18 @@
 <template>
   <v-list-item>
     <v-list-item-title v-html="title" />
-    <v-list-item-subtitle v-if="text" class="wordwrap" v-html="text" />
-    <v-list-item-subtitle v-if="subText" class="wordwrap" v-html="subText" />
+    <v-list-item-subtitle v-if="bullet && subtitles.length" class="wordwrap">
+      <ul class="pl-4 my-1">
+        <li v-for="(line, index) in subtitles" :key="index" v-html="line" />
+      </ul>
+    </v-list-item-subtitle>
+    <v-list-item-subtitle
+      v-else
+      v-for="(line, index) in subtitles"
+      :key="index"
+      class="wordwrap"
+      v-html="line"
+    />
     <template v-if="avHref && (avIcon || avImg)" v-slot:append>
       <avatar :href="avHref" :tooltip="avTooltip" :icon="avIcon" :img="avImg" />
     </template>
@@ -10,7 +20,9 @@
 </template>
 
 <script setup lang="ts">
-defineProps({
+import { computed, type PropType } from "vue";
+
+const props = defineProps({
   title: {
     type: String,
     required: true,
@@ -22,6 +34,14 @@ defineProps({
   subText: {
     type: String,
     default: "",
+  },
+  texts: {
+    type: Array as PropType<string[]>,
+    default: () => [],
+  },
+  bullet: {
+    type: Boolean,
+    default: false,
   },
   avHref: {
     type: String,
@@ -39,5 +59,14 @@ defineProps({
     type: String,
     default: "",
   },
+});
+
+const subtitles = computed(() => {
+  if (props.texts.length) return props.texts;
+
+  const subtitles: string[] = [];
+  if (props.text) subtitles.push(props.text);
+  if (props.subText) subtitles.push(props.subText);
+  return subtitles;
 });
 </script>
--- src/views/index/ProfileCard.vue
+++ src/views/index/ProfileCard.vue
@@ -12,44 +12,50 @@
           av-img="/img/x.webp"
           av-tooltip="X"
         />
-        <subheader text="出身校" />
-        <list-item
-          title="旭川工業高等専門学校"
-          text="制御情報工学科 卒業"
-          sub-text="高専ロボコン出場（機械設計/加工/プログラミング）"
-        />
-        <list-item title="電気通信大学" text="情報工学科 卒業" />
-        <list-item
-          title="電気通信大学 大学院"
-          text="情報工学専攻 修了"
-          sub-text="工学修士"
-        />
         <subheader text="職歴" class="mt-8" />
         <list-item
           title="ソフトバンク(株)"
-          text="営業施策分析、契約者数予測、Azure/AWSでのデータ分析システム企画・開発・運用、スポーツデータの機械学習分析など"
-          sub-text="<small>※ウィルコム入社後 吸収合併</small>"
+          :texts="[
+            '営業施策分析、契約者数予測、データ分析システム企画・開発・運用、スポーツデータの機械学習分析など',
+            '<small>※ウィルコム入社後 吸収合併</small>',
+          ]"
         />
         <list-item
           title="ai6(株)"
-          text="スタートアップにて開発全般担当。PoC用ウェブアプリ、PoC用デバイスファームウェア用アプリ、サーバ、データレイク開発、データ分析等。"
-          sub-text=" <small>General Manager, Engineering, 現職</small>"
+          :texts="[
+            'スタートアップにて開発全般担当。PoC用ウェブアプリ、デバイス関連開発、クラウドシステム、データレイク開発、データ分析等。',
+            '<small>テックリード, 現職</small>',
+          ]"
           av-href="https://ai6.jp/"
           av-icon="$mdiBrain"
           av-tooltip="ai6 株式会社"
         />
         <list-item
           title="CoCoDakara(株)"
-          text="麻布十番のパーソナルジム「CoCoDakara Body Design」の予約システム開発・運用担当。"
-          sub-text="<small>取締役, 現職</small>"
+          :texts="[
+            '麻布十番のパーソナルジム「CoCoDakara Body Design」の予約システム開発・運用担当。',
+            '<small>取締役, 現職</small>',
+          ]"
           av-href="https://bodydesign.cocodakara.net/"
           av-icon="$mdiDumbbell"
           av-tooltip="CoCoDakara BodyDesign"
         />
         <list-item
           title="ITフリーランス"
-          text="Webアプリ開発等"
-          sub-text="<small>現職</small>"
+          :texts="['Webアプリ開発等', '<small>現職</small>']"
+        />
+        <subheader text="学歴" />
+        <list-item
+          title="旭川工業高等専門学校"
+          :texts="[
+            '制御情報工学科 卒業',
+            'NHK高専ロボコン出場（機械設計/加工/プログラミング）',
+          ]"
+        />
+        <list-item title="電気通信大学" text="情報工学科 卒業" />
+        <list-item
+          title="電気通信大学 大学院"
+          :texts="['情報工学専攻 修了', '工学修士']"
         />
       </v-list>
     </v-card-text>
